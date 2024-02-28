74801e623197044ae8392017840886e8a84274e7
Kgotso mnguni <kgotsomnguni09@gmail.com>
2024-02-28 17:06:26 +0200

    creating a transactions class that will handle all the necessary transaction modules

---
+++ Transactions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceTrackerCons
+{
+    internal class Transactions
+    {
+        public string Description { get; set; }
+        public int amount { get; set; }
+    }
+}
